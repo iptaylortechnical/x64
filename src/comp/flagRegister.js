07f32366a5088064962d3f02d0f9581af227c8ce
Isaiah Taylor <iptaylortechnical@gmail.com>
2019-04-05 11:37:12 -0700

    flag changes

--- src/comp/flagRegister.js
+++ src/comp/flagRegister.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import './css/flagRegister.css';
-import BinaryString from './binaryString';
-import HexString from './hexString';
 
 class FlagRegister extends Component {
   render() {
+    const keys = Object.keys(this.props.flags);
+    const first = keys.slice(0, 6).map(k => { return { key: k, value: this.props.flags[k] } });
+    const second = keys.slice(6, 12).map(k => { return { key: k, value: this.props.flags[k] } });
+    const third = keys.slice(12, keys.length).map(k => { return { key: k, value: this.props.flags[k] } });
 
     return (
       <div className="FlagRegister Register">
-        {/* <BinaryString value={base2String}/>
-        <HexString value={base16String}/>
-        <div className="decimalValue">{value}</div> */}
-
-        {Object.keys(this.props.flags)
-          .map(k => <Flag value={this.props.flags[k]}
-            description={k} />)}
-
+        <div className="flagRow">
+          {first
+            .map(k => <Flag value={k.value}
+              description={k.key} />)}
+        </div>
+        <div className="flagRow">
+          {second
+            .map(k => <Flag value={k.value}
+              description={k.key} />)}
+        </div>
+        <div className="flagRow">
+          {third
+            .map(k => <Flag value={k.value}
+              description={k.key} />)}
+        </div>
         <div className="ident">{this.props.label}</div>
       </div>
     )
@@ -26,8 +35,8 @@ class Flag extends Component {
   render() {
     return (
       <div className="Flag">
-        <div>{this.props.value}</div>
-        <div>{this.props.description}</div>
+        <div className="flagDescription">{this.props.description}</div>
+        <div className="flagValue">{this.props.value}</div>
       </div>
     )
   }
